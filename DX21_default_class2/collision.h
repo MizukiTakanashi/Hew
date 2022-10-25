99c50073842ffa50977ddf027ace1c5fef4fb2ea
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2022-10-25 15:16:41 +0900

    コメント追加しました

--- DX21_default_class2/collision.h
+++ DX21_default_class2/collision.h
@@ -19,10 +19,14 @@ public:
 	~Collision(){}	//デストラクタ
 
 	//四角形同士の当たり判定 true:当たった false:当たってない
+	//pos1：オブジェクト1の座標　pos2：オブジェクト2の座標　
+	//size1：オブジェクト1のサイズ　size2：オブジェクト2のサイズ
 	static bool ColBox(const D3DXVECTOR2& pos1, const D3DXVECTOR2& pos2, 
 		const D3DXVECTOR2& size1, const D3DXVECTOR2& size2);
 
 	//丸同士の当たり判定 true:当たった false:当たってない
+	//pos1：オブジェクト1の座標　pos2：オブジェクト2の座標
+	//rot1：オブジェクト1の半径　rot2：オブジェクト2の半径
 	static bool ColCircle(const D3DXVECTOR2& pos1, const D3DXVECTOR2& pos2, float rot1, float rot2);
 };
 
