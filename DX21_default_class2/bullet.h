157159efc7b38d350a362ebdd1d862c2564cb99f
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2022-10-28 18:14:19 +0900

    敵1の弾がホーミングになりました

--- DX21_default_class2/bullet.h
+++ DX21_default_class2/bullet.h
@@ -7,13 +7,15 @@
 #ifndef _BULLET_H_
 #define _BULLET_H_
 
+#include <time.h>
 #include "GameObject.h"
 #include "DrawObject.h"
 
 class Bullet:public GameObject
 {
 private:
-	D3DXVECTOR2 m_mov = D3DXVECTOR2(0.0f, 0.0f);
+	D3DXVECTOR2 m_mov = D3DXVECTOR2(0.0f, 0.0f);	//移動量
+	int m_time = 0;									//弾が出来てからの経過時間
 
 public:
 	Bullet(){}	//デフォルトコンストラクタ
@@ -24,8 +26,14 @@ public:
 
 	virtual ~Bullet() {}	//デストラクタ
 
-	//更新処理(弾を移動)
-	void Update(void){ GameObject::MovePos(m_mov); }
+	//更新処理(弾を移動)(経過時間を計測)
+	void Update(void) { GameObject::MovePos(m_mov); m_time++; }
+
+	//弾の移動を再セット(ホーミング弾用かな？)
+	void SetMove(const D3DXVECTOR2& mov) { m_mov = mov; }
+
+	//弾が出来てからの経過時間を返す
+	int GetTime(void)const { return m_time; }
 };
 
 #endif // !_BULLET_H_
