df96e2071263fd8594f2388a41e0db46970df9b9
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2022-10-28 14:44:53 +0900

    色々と継承用のクラスを作りました

---
+++ DX21_default_class2/PlayerArm1.h
@@ -0,0 +1,23 @@
+#pragma once
+//=======================================
+// プレイヤーの腕1関係(ヘッダファイル)
+// 作成日：2022/10/28
+// 作成者：高梨水希
+//=======================================
+
+#ifndef _PLAYER_ARM_1_H_
+#define _PLAYER_ARM_1_H_
+
+#include "inhPlayerArm.h"
+
+class PlayerArm1:public inhPlayerArm
+{
+public:
+	//デフォルトコンストラクタ
+	PlayerArm1(){}
+
+	//デストラクタ
+	~PlayerArm1(){}
+};
+
+#endif // !_PLAYER_ARM_1_H_
