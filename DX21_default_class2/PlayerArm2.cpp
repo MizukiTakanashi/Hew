1c450e70120066a929abf7bc8e1cd27b17e2ff99
yanotin <zatsuo.0731@gmail.com>
2022-11-02 00:37:23 +0900

    m_pBulletに変更

--- DX21_default_class2/PlayerArm2.cpp
+++ DX21_default_class2/PlayerArm2.cpp
@@ -13,30 +13,31 @@ void PlayerArm2::Update()
 {
 	//発射できる時間になったら...
 	if (++m_bullet_interval_count > BULLET_INTERVAL) {
+		for (int i = 0; i < m_bullet_num; i++) {
+			//レーザーの残り時間減少
+			m_lasertime--;
 
-		//レーザーの残り時間減少
-		m_lasertime--;
-
-		if (GameObject::GetSize().y > SCREEN_HEIGHT) //レーザーが十分な長さになったか
-		{
-			if (m_lasertime < 0) //レーザーの射出時間が終わったか
+			if (m_pBullet[i].GetSize().y > SCREEN_HEIGHT) //レーザーが十分な長さになったか
 			{
-				//レーザーを短くする
-				GameObject::SetSize(D3DXVECTOR2(GameObject::GetSize().x, GameObject::GetSize().y - BULLET_SPEED * 2));
+				if (m_lasertime < 0) //レーザーの射出時間が終わったか
+				{
+					//レーザーを短くする
+					m_pBullet[i].SetSize(D3DXVECTOR2(m_pBullet[i].GetSize().x, m_pBullet[i].GetSize().y - BULLET_SPEED * 2));
+				}
+				//レーザーのX座標をプレイヤーに合わせる
+				m_pBullet[i].SetPos(D3DXVECTOR2(m_pPlayer->GetPos().x, m_pBullet[i].GetPos().y));
 			}
-			//レーザーのX座標をプレイヤーに合わせる
-			GameObject::SetPos(D3DXVECTOR2(m_pPlayer->GetPos().x, GameObject::GetPos().y));
-		}
-		else
-		{
-			//レーザーを長くする
-			GameObject::SetSize(D3DXVECTOR2(GameObject::GetSize().x, GameObject::GetSize().y - BULLET_SPEED * 2));
-			//レーザーのX座標をエネミーに合わせる
-			GameObject::SetPos(D3DXVECTOR2(m_pPlayer->GetPos().x, GameObject::GetPos().y - BULLET_SPEED));
-		}
+			else
+			{
+				//レーザーを長くする
+				m_pBullet[i].SetSize(D3DXVECTOR2(m_pBullet[i].GetSize().x, m_pBullet[i].GetSize().y - BULLET_SPEED * 2));
+				//レーザーのX座標をエネミーに合わせる
+				m_pBullet[i].SetPos(D3DXVECTOR2(m_pPlayer->GetPos().x, m_pBullet[i].GetPos().y - BULLET_SPEED));
+			}
 
-		//現在の弾の数を増やす
-		m_bullet_num++;
+			//現在の弾の数を増やす
+			m_bullet_num++;
+		}
 	}
 }
 
