cf599b8bc4b154e357fa91de86730a5a3986b146
HiroyukiOnda <halon425@outlook.com>
2022-11-02 10:25:32 +0900

    レーザーの射出が終わった後ｘ座標が固定されるように修正

--- DX21_default_class2/laser.cpp
+++ DX21_default_class2/laser.cpp
@@ -11,21 +11,15 @@
 const float Laser::SPEED_Y = 15.0f;
 const int Laser::LASER_TIME = 150;
 
-
 void Laser::Update(void)
 {
 	//レーザーの残り時間減少
 	m_lasertime--;
 
-	if (GameObject::GetSize().y > SCREEN_HEIGHT) //レーザーが十分な長さになったか
+	if (m_lasertime < 0) //レーザーの射出時間が終わったか
 	{
-		if (m_lasertime < 0) //レーザーの射出時間が終わったか
-		{
-			//レーザーを短くする
-			GameObject::SetSize(D3DXVECTOR2(GameObject::GetSize().x, GameObject::GetSize().y - SPEED_Y * 2));
-		}
-		//レーザーのX座標をエネミーに合わせる
-		GameObject::SetPos(D3DXVECTOR2(m_pEnemynum->GetPos().x, GameObject::GetPos().y));
+		//レーザーの移動
+		GameObject::SetPos(D3DXVECTOR2(GameObject::GetPos().x, GameObject::GetPos().y + SPEED_Y));
 	}
 	else
 	{
@@ -34,6 +28,5 @@ void Laser::Update(void)
 		//レーザーのX座標をエネミーに合わせる
 		GameObject::SetPos(D3DXVECTOR2(m_pEnemynum->GetPos().x, GameObject::GetPos().y + SPEED_Y));
 	}
-
 
 }
