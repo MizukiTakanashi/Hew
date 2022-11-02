91f9edb5f41a6dc1dfd7c9ff0094d3a232e9b72e
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2022-11-02 15:48:32 +0900

    発射間隔をつけました

--- DX21_default_class2/PlayerArm1.h
+++ DX21_default_class2/PlayerArm1.h
@@ -17,7 +17,8 @@ class PlayerArm1:public inhPlayerArm
 //定数
 private:
 	//ここで初期化
-	static const int BULLET_NUM_MAX = 10;		//弾の同時最大発射数
+	static const int BULLET_NUM_MAX = 20;		//弾の制限数
+	static const int BULLET_SHOOT_MAX = 10;		//弾の同時最大発射数
 	static const int BULLET_INTERVAL = 20;		//弾の発射間隔
 	static const int BULLET_BREAK_TIME = 200;	//ホーミング弾が壊れる時間
 
@@ -30,17 +31,18 @@ private:
 private:
 	DrawObject m_bulletdraw;			//弾の描画オブジェクト
 	Bullet* m_pBullet = nullptr;		//弾のオブジェクト
+	int m_interval_count = 0;			//発射間隔のカウント
 	bool m_right = true;				//自分がついているのが右か左か判断
 	D3DXVECTOR2 m_enemy_pos = D3DXVECTOR2(0.0f, 0.0f);	//一番近い敵の位置
 
 //メンバ関数
 public:
 	//デフォルトコンストラクタ
-	PlayerArm1(){ m_pBullet = new Bullet[BULLET_NUM_MAX]; }
+	PlayerArm1(){ m_pBullet = new Bullet[BULLET_SHOOT_MAX]; }
 
 	//引数付きコンストラクタ
 	PlayerArm1(DrawObject bulletdraw, bool right)
-		:m_bulletdraw(bulletdraw), m_right(right) { m_pBullet = new Bullet[BULLET_NUM_MAX]; }
+		:inhPlayerArm(BULLET_NUM_MAX), m_bulletdraw(bulletdraw), m_right(right) { m_pBullet = new Bullet[BULLET_SHOOT_MAX]; }
 
 	//デストラクタ
 	~PlayerArm1() { delete[] m_pBullet; }
