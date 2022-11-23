8c663f4e5b678fb4b11fa3f2c2dc7124fd46e52f
yanotin <zatsuo.0731@gmail.com>
2022-11-23 22:29:11 +0900

    爆弾基礎

---
+++ DX21_default_class2/Bom.cpp
@@ -0,0 +1,1 @@
+#include "Bom.h"
---
+++ DX21_default_class2/Bom.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <time.h>
+#include "game_object.h"
+#include "draw_object.h"
+class Bom :public GameObject
+{
+private:
+	D3DXVECTOR2 m_mov = D3DXVECTOR2(0.0f, 0.0f);	//移動量
+	int m_time = 0;									//弾が出来てからの経過時間
+
+public:
+	Bom() {}	//デフォルトコンストラクタ
+
+	//引数付きコンストラクタ
+	Bom(DrawObject& pDrawObject, const D3DXVECTOR2& pos, const D3DXVECTOR2& size, const D3DXVECTOR2& mov, float rot)
+		:GameObject(pDrawObject, pos, size, rot), m_mov(mov) {}
+
+	~Bom()override {}	//デストラクタ
+
+	//更新処理(弾を移動)(経過時間を計測)
+	void Update(void) { GameObject::MovePos(m_mov); m_time++; }
+
+	//弾の移動を再セット(ホーミング弾用かな？)
+	void SetMove(const D3DXVECTOR2& mov) { m_mov = mov; }
+
+	//弾が出来てからの経過時間を返す
+	int GetTime(void)const { return m_time; }
+};
+
