d25f0afb258d8116617da656670440a0aeb2e2fe
HiroyukiOnda <halon425@outlook.com>
2022-12-13 11:43:40 +0900

    a

--- DX21_default_class2/enemy_normal.cpp
+++ DX21_default_class2/enemy_normal.cpp
@@ -16,7 +16,6 @@ const float EnemyNormal::RANGE = 20.0f;
 //private
 const float EnemyNormal::SPEED_X = 1.5f;
 const float EnemyNormal::SPEED_Y = 2.5f;
-const int EnemyNormal::HP_MAX = 2;
 
 //======================
 // XVˆ—
