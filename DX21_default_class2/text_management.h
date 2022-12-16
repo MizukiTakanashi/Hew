9abeff2d3d4035ea8c75ed46a0690bd34855e787
HiroyukiOnda <halon425@outlook.com>
2022-12-16 16:08:21 +0900

    asf

---
+++ DX21_default_class2/text_management.h
@@ -0,0 +1,25 @@
+//====================================================
+// テキストの管理関係(ヘッダファイル)
+// 作成日：
+// 作成者：恩田洋行
+//====================================================
+#pragma once
+
+
+class TextManagement
+{
+//定数
+public:
+	const static float FLAME_SIZE_X; //フレームサイズ
+	const static float FLAME_SIZE_Y; //フレームサイズ
+
+private:
+
+//メンバ変数
+private:
+
+
+//メンバ関数
+public:
+
+};
