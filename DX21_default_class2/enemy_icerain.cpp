a401a7bbc983954b0b3efd91d1c9933d8f3e1e74
yanotin <zatsuo.0731@gmail.com>
2022-12-22 15:36:48 +0900

    氷の敵

--- DX21_default_class2/enemy_icerain.cpp
+++ DX21_default_class2/enemy_icerain.cpp
@@ -1,9 +1,10 @@
 //=======================================
-// •’Ê‚Ì“GŠÖŒW(cppƒtƒ@ƒCƒ‹)
-// ì¬“úF2022/09/13
-// ì¬ŽÒF‚—œ…Šó
+// •X‚Ì“GŠÖŒW(cppƒtƒ@ƒCƒ‹)
+// ì¬“úF2022/12/19
+// ì¬ŽÒF–î–ìãÄ‘å
 //=======================================
 #include "enemy_icerain.h"
+
 //==========================
 // ’è”‰Šú‰»
 //==========================
@@ -16,6 +17,11 @@ const float EnemyIceRain::RANGE = 20.0f;
 const float EnemyIceRain::SPEED_X = 1.5f;
 const float EnemyIceRain::SPEED_Y = 2.5f;
 
+int m_count = 0;
+int m_count1 = 0;
+int m_count2 = 0;
+int m_count3 = 0;
+int m_count4 = 0;
 //======================
 // XVˆ—
 //======================
@@ -31,10 +37,31 @@ void EnemyIceRain::Update(void)
 	float rad = D3DXToRadian(m_move_width);
 	SetPos(D3DXVECTOR2(m_init_posx + cosf(rad) * RANGE, GetPos().y));
 	m_move_width += SPEED_X;
-
+	m_count++;
+	m_count1++;
+	m_count2++;
+	m_count3++;
+	m_count4++;
 	//ŽžŠÔ‚ª—ˆ‚½‚ç’e‚ðì‚é
-	if (m_bullet_count++ > BULLET_TIME) {
+	if (m_bullet_count> BULLET_TIME) {
 		m_bullet_make = true;
+		m_count = 0;
+	}
+	if (m_count1 > BULLET_TIME+1) {
+		m_bullet_make1 = true;
+		m_count1 = 0;
+	}
+	if (m_count2 > BULLET_TIME+2) {
+		m_bullet_make2 = true;
+		m_count2 = 0;
+	}
+	if (m_count3 > BULLET_TIME+3) {
+		m_bullet_make3 = true;
+		m_count3 = 0;
+	}
+	if (m_count4 > BULLET_TIME+4) {
+		m_bullet_make4 = true;
 		m_bullet_count = 0;
+		m_count4 = 0;
 	}
 }
